80f7efbf0d40b58b2eb792df5d40263dac430530
Ilya Burinsky <i.burinsky@sftpro.ru>
2022-02-21 22:29:31 +0300

    Reading file from argv

--- calculator.c
+++ calculator.c
@@ -1,6 +1,12 @@
+#include <stdio.h>
+
 int scan_tokens();
 
-int main(int args, char ** argv) {
+int main(int argc, char ** argv) {
+  if (argc >= 2) {
+    extern FILE * yyin;
+    yyin = fopen(argv[1], "r");
+  }
   scan_tokens();
   return 0;
 }
