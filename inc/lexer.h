e4b8dfb7576fcd35bfbb28247fb404edf2e6cc05
Ilya Burinsky <i.burinsky@sftpro.ru>
2022-03-01 12:03:24 +0300

    Added ifndef guard in lexer.h

--- inc/lexer.h
+++ inc/lexer.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <stdio.h>
 
-struct yy_buffer_state;
+#ifndef YY_TYPEDEF_YY_BUFFER_STATE
 #define YY_TYPEDEF_YY_BUFFER_STATE
 typedef struct yy_buffer_state * YY_BUFFER_STATE;
+#endif
 
+#ifndef YY_BUF_SIZE
 #define YY_BUF_SIZE 16384
+#endif
 
 YY_BUFFER_STATE yy_create_buffer(FILE * file, int size);
 void yy_switch_to_buffer(YY_BUFFER_STATE new_buffer);
