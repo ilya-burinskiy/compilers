60ad24f536281d45ff17a53955b1dc2456d8c032
Ilya Burinsky <i.burinsky@sftpro.ru>
2022-04-18 23:06:35 +0300

    Removed Statements struct

--- inc/abstract_syntax.h
+++ inc/abstract_syntax.h
@@ -1,10 +1,5 @@
 #pragma once
 
-typedef struct Statements {
-  struct Statements * stmts;
-  struct Statement * stmt;
-} Statements;
-
 typedef struct Statement {
   int lineno; /* temporary value */
 } Statement;
