2e5b04e0246d10d55f2747ac480c3684bf6e4bd7
Ilya Burinsky <i.burinsky@sftpro.ru>
2022-04-19 22:37:18 +0300

    BinOp and Expression types

--- inc/abstract_syntax.h
+++ inc/abstract_syntax.h
@@ -7,4 +7,14 @@ struct Statement {
   } u;
 };
 
+enum BinOp { LOR_BINOP, LXOR_BINOP, LAND_BINOP };
+struct Expression {
+  enum { BINOP_EXPR } type;
+  union {
+    struct { struct Expression * left; enum BinOp binop; struct Expression * right; } binop_expr;
+  } u;
+};
+
+typedef enum BinOp BinOp;
+typedef struct Expression Expression;
 typedef struct Statement Statement;
