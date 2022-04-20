540952142ece5aa9ab420feed26e29b0fede5a3f
Ilya Burinsky <i.burinsky@sftpro.ru>
2022-04-20 19:52:30 +0300

    Using typedef when defining structs

--- inc/abstract_syntax.h
+++ inc/abstract_syntax.h
@@ -2,22 +2,18 @@
 
 struct Expression;
 
-struct Statement {
+typedef struct Statement {
   enum { COMPOUND_STMT, EXPRESSION_STMT } type;
   union {
     struct { struct Statement * stmt1, * stmt2; } compound;
     struct Expression * expr;
   } u;
-};
+} Statement;
 
-enum BinOp { LOR_BINOP, LXOR_BINOP, LAND_BINOP };
-struct Expression {
+typedef enum BinOp { LOR_BINOP, LXOR_BINOP, LAND_BINOP } BinOp;
+typedef struct Expression {
   enum { BINOP_EXPR, TRUE_EXPR, FALSE_EXPR } type;
   union {
     struct { struct Expression * left; enum BinOp binop; struct Expression * right; } binop_expr;
   } u;
-};
-
-typedef enum BinOp BinOp;
-typedef struct Expression Expression;
-typedef struct Statement Statement;
+} Expression;
