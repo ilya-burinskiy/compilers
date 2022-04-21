b17fa111f223017ce8aee4cef0ac44763592078e
Ilya Burinsky <i.burinsky@sftpro.ru>
2022-04-21 23:14:58 +0300

    Ignoring \n if it followed another \n

--- src/lexer.c
+++ src/lexer.c
@@ -375,8 +375,8 @@ static void yy_fatal_error (yyconst char msg[]  );
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
 
-#define YY_NUM_RULES 15
-#define YY_END_OF_BUFFER 16
+#define YY_NUM_RULES 16
+#define YY_END_OF_BUFFER 17
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -384,11 +384,11 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[24] =
+static yyconst flex_int16_t yy_accept[27] =
     {   0,
-        0,    0,   16,   12,   14,   13,   11,   15,    4,    5,
-        6,    7,    3,    1,    2,   10,   15,   12,   14,    9,
-        3,    8,    0
+        0,    0,    0,    0,   17,   12,   13,   16,   11,   13,
+        4,    5,    6,    7,    3,    1,    2,   10,   13,   14,
+       12,   15,    9,    3,    8,    0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
@@ -429,42 +429,42 @@ static yyconst flex_int32_t yy_meta[16] =
         2,    2,    2,    1,    1
     } ;
 
-static yyconst flex_int16_t yy_base[25] =
+static yyconst flex_int16_t yy_base[28] =
     {   0,
-        0,    0,   23,   15,   20,   26,   26,   16,   26,   26,
-       26,   26,    0,    0,    0,   26,    5,   16,   17,   26,
-        0,   26,   26,   16
+        0,    0,   24,   23,   23,   15,   20,   28,   28,   16,
+       28,   28,   28,   28,    0,    0,    0,   28,    5,   28,
+       16,   17,   28,    0,   28,   28,   16
     } ;
 
-static yyconst flex_int16_t yy_def[25] =
+static yyconst flex_int16_t yy_def[28] =
     {   0,
-       23,    1,   23,   23,   23,   23,   23,   23,   23,   23,
-       23,   23,   24,   24,   24,   23,   23,   23,   23,   23,
-       24,   23,    0,   23
+       26,    1,    1,    1,   26,   26,   26,   26,   26,   26,
+       26,   26,   26,   26,   27,   27,   27,   26,   26,   26,
+       26,   26,   26,   27,   26,    0,   26
     } ;
 
-static yyconst flex_int16_t yy_nxt[42] =
+static yyconst flex_int16_t yy_nxt[44] =
     {   0,
-        4,    5,    6,    7,    8,    9,   10,   11,    4,   12,
-       13,   14,   15,   16,   17,   18,   18,   21,   19,   22,
-       20,   19,   23,   18,   18,    3,   23,   23,   23,   23,
-       23,   23,   23,   23,   23,   23,   23,   23,   23,   23,
-       23
+        6,    7,    8,    9,   10,   11,   12,   13,    6,   14,
+       15,   16,   17,   18,   19,   21,   21,   24,   22,   25,
+       23,   22,   26,   21,   21,   20,   20,    5,   26,   26,
+       26,   26,   26,   26,   26,   26,   26,   26,   26,   26,
+       26,   26,   26
     } ;
 
-static yyconst flex_int16_t yy_chk[42] =
+static yyconst flex_int16_t yy_chk[44] =
     {   0,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    4,   18,   24,   19,   17,
-        8,    5,    3,    4,   18,   23,   23,   23,   23,   23,
-       23,   23,   23,   23,   23,   23,   23,   23,   23,   23,
-       23
+        1,    1,    1,    1,    1,    6,   21,   27,   22,   19,
+       10,    7,    5,    6,   21,    4,    3,   26,   26,   26,
+       26,   26,   26,   26,   26,   26,   26,   26,   26,   26,
+       26,   26,   26
     } ;
 
 /* Table of booleans, true if rule could match eol. */
-static yyconst flex_int32_t yy_rule_can_match_eol[16] =
+static yyconst flex_int32_t yy_rule_can_match_eol[17] =
     {   0,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,     };
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,     };
 
 static yy_state_type yy_last_accepting_state;
 static char *yy_last_accepting_cpos;
@@ -481,13 +481,15 @@ int yy_flex_debug = 0;
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
 #line 1 "src/lexer.l"
-#line 5 "src/lexer.l"
+
+#line 7 "src/lexer.l"
 #include <stdio.h>
 #include <stdlib.h>
 #include "tokens.h"
-#line 489 "src/lexer.c"
+#line 490 "src/lexer.c"
 
 #define INITIAL 0
+#define consumed_non_new_line_char 1
 
 #ifndef YY_NO_UNISTD_H
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -667,9 +669,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
-#line 25 "src/lexer.l"
+#line 27 "src/lexer.l"
 
-#line 673 "src/lexer.c"
+#line 675 "src/lexer.c"
 
 	if ( !(yy_init) )
 		{
@@ -722,13 +724,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 24 )
+				if ( yy_current_state >= 27 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 26 );
+		while ( yy_base[yy_current_state] != 28 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -764,82 +766,88 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 26 "src/lexer.l"
+#line 28 "src/lexer.l"
 { return FALSE; }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 27 "src/lexer.l"
+#line 29 "src/lexer.l"
 { return TRUE; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 28 "src/lexer.l"
+#line 30 "src/lexer.l"
 { yylval.id = strdup(yytext); return IDENTIFIER; }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 29 "src/lexer.l"
+#line 31 "src/lexer.l"
 { return LPARENT; }
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 30 "src/lexer.l"
+#line 32 "src/lexer.l"
 { return RPARENT; }
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 31 "src/lexer.l"
+#line 33 "src/lexer.l"
 { return COMMA; }
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 32 "src/lexer.l"
+#line 34 "src/lexer.l"
 { return ASSIGN; }
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 33 "src/lexer.l"
+#line 35 "src/lexer.l"
 { return LOR; }
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 34 "src/lexer.l"
+#line 36 "src/lexer.l"
 { return LAND; }
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 35 "src/lexer.l"
+#line 37 "src/lexer.l"
 { return LXOR; }
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 36 "src/lexer.l"
+#line 38 "src/lexer.l"
 { return LNOT; }
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 37 "src/lexer.l"
+#line 39 "src/lexer.l"
 { return ERROR; }
 	YY_BREAK
 case 13:
-/* rule 13 can match eol */
-YY_RULE_SETUP
-#line 39 "src/lexer.l"
-{ return NEW_LINE; }
-	YY_BREAK
-case 14:
 YY_RULE_SETUP
 #line 40 "src/lexer.l"
-
+{ BEGIN(consumed_non_new_line_char); }
+	YY_BREAK
+case 14:
+/* rule 14 can match eol */
+YY_RULE_SETUP
+#line 42 "src/lexer.l"
+{ BEGIN(INITIAL); return NEW_LINE; }
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 41 "src/lexer.l"
+#line 43 "src/lexer.l"
+
+	YY_BREAK
+case 16:
+YY_RULE_SETUP
+#line 44 "src/lexer.l"
 ECHO;
 	YY_BREAK
-#line 842 "src/lexer.c"
+#line 849 "src/lexer.c"
 case YY_STATE_EOF(INITIAL):
+case YY_STATE_EOF(consumed_non_new_line_char):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -1130,7 +1138,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 24 )
+			if ( yy_current_state >= 27 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1158,11 +1166,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 24 )
+		if ( yy_current_state >= 27 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 23);
+	yy_is_jam = (yy_current_state == 26);
 
 	return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1847,4 +1855,4 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 41 "src/lexer.l"
+#line 44 "src/lexer.l"
